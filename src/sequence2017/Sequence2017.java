16f55028daf311a50576fc6e138eb3a8a1458433
Nick Ivanov <nnrowan@gmail.com>
2017-11-30 20:15:24 -0600

    Using Double instead of Integer

--- src/sequence2017/Sequence2017.java
+++ src/sequence2017/Sequence2017.java
@@ -8,26 +8,32 @@ import java.util.Arrays;
 
 public class Sequence2017 {
     
-    private static int getExpression(ArrayList<Integer> a, int n) {
+    private static double getExpression(ArrayList<Double> a, int n) {
         // MODIFY POINT 2
-        return a.get(n-1) + a.get(n-2);
+        return a.get(n-1) * (1.0+0.08) - 3000.0;
     }
 
     public static void main(String[] args) {
         // OPTIONAL MODIFY POINT
-        final int N = 20;
-        ArrayList<Integer> a = new ArrayList<>(N);
+        final int N = 80;
+        ArrayList<Double> a = new ArrayList<>(N);
         
         // MODIFY POINT 1
-        ArrayList<Integer> init = new ArrayList(Arrays.asList(0, 1));
+        ArrayList<Double> init = new ArrayList(Arrays.asList(30000.0));
         
-        for(int i = 0; i < init.size(); i++) {
+        int i = 0;
+        for(i = 0; i < init.size(); i++) {
             a.add(i, init.get(i));
         }
                 
-        for(int i = init.size(); i < N; i++) {
+        for(i = init.size(); i < N; i++) {
             a.add(i, getExpression(a, i));
+            if(getExpression(a, i) <= 0) {
+                break;
+            }
         }
+        
+        System.out.println(i);
         
         System.out.println(a.toString());
     }
